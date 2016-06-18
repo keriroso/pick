19789f21c040ec4016aa7a20364a3a04ae02afc9
Miguel Medina MacBookPro <miguelo.aja@gmail.com>
2016-06-18 15:48:59 -0500

    geolocalizacion cambios en limpiar cache de gps

--- www/js/geolocation.js
+++ www/js/geolocation.js
@@ -1,32 +1,40 @@
 angular
     .module('pickplace.geolocation', ['ngGeolocation'])
     .controller('geolocCtrl', ['$geolocation', '$scope', function($geolocation, $scope) {
+      $scope.mostrar_url_ubicacion = false;
+      $scope.mostrar_boton_gps = false;
          $geolocation.getCurrentPosition({
             timeout: 60000
          }).then(function(position) {
             $scope.myPosition = position;
-            console.log('Tu posición : ' + $scope.myPosition );
+            console.log('Tu posicion : ', $scope.myPosition );
+            $scope.mostrar_boton_gps = true;
          });
          
         
         
         $scope.obtenerActualPosicion = function() {
+          
           $geolocation.watchPosition({
             timeout: 60000,
             maximumAge: 250,
             enableHighAccuracy: true
           });
-          $scope.myCoords = $geolocation.position.coords; // this is regularly updated
-          $scope.myError = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
+          
+          $scope.myCoords = $geolocation.position.coords; // Se actualiza regularmente
+          $scope.myError = $geolocation.position.error; // Error
           
           console.log('$scope.myCoords : ' , $scope.myCoords);
           console.log('$scope.myError : ' , $scope.myError);
-          var url_google_maps = 'https://www.google.com.ec/maps/@';
-          url_google_maps += $scope.myCoords.latitude + ',';
-          url_google_maps += $scope.myCoords.longitude + ',';
-          url_google_maps += $scope.myCoords.accuracy + '19z';
           
-          console.log('url_google_maps : ' + url_google_maps);
+          // Armar la URL de Google maps
+          $scope.url_google_maps = 'https://www.google.com.ec/maps/@';
+          $scope.url_google_maps += $scope.myCoords.latitude + ',';
+          $scope.url_google_maps += $scope.myCoords.longitude + ',';
+          $scope.url_google_maps += $scope.myCoords.accuracy + '28z';
+          
+          console.log('url_google_maps : ' + $scope.url_google_maps);
+          $scope.mostrar_url_ubicacion = true;
         };
         
       
